39a341667a885676655b007cd4a515c89818ef7d
Newsha Nik <nioosha.h@gmail.com>
2020-08-17 18:28:47 +0430

    added dropdown-autocomplete-countries component to project

---
+++ src/app/widget/components/dropdown-autocomplete-countries/dropdown-autocomplete-countries.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dropdown-autocomplete-countries',
+  templateUrl: './dropdown-autocomplete-countries.component.html',
+  styleUrls: ['./dropdown-autocomplete-countries.component.scss'],
+})
+export class DropdownAutocompleteCountriesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
