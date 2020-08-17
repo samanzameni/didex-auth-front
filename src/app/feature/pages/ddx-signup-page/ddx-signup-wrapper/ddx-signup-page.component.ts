92f01c08c207f5e10b1ef964e0775051e28e2342
Newsha Nik <nioosha.h@gmail.com>
2020-08-17 15:06:55 +0430

    added a bunch of folders for sign up component. fixed the errors. gotta change the name and insert the routers now

--- src/app/feature/pages/ddx-signup-page/ddx-signup-wrapper/ddx-signup-page.component.ts
+++ src/app/feature/pages/ddx-signup-page/ddx-signup-wrapper/ddx-signup-page.component.ts
@@ -19,7 +19,7 @@ import { environment } from '@environments/environment';
   selector: 'ddx-signup-page',
   templateUrl: './ddx-signup-page.component.html',
   styleUrls: [
-    '../../public/ddx-auth-pages.scss',
+    '../../../public/ddx-auth-pages.scss',
     './ddx-signup-page.component.scss',
   ],
 })
--- src/app/feature/pages/index.ts
+++ src/app/feature/pages/index.ts
@@ -2,6 +2,6 @@ export * from './ddx-activate-email-page/ddx-activate-email-page.component';
 export * from './ddx-forgot-password-page/ddx-forgot-password-page.component';
 export * from './ddx-reset-password-page/ddx-reset-password-page.component';
 export * from './ddx-signin-page/ddx-signin-page.component';
-export * from './ddx-signup-page/ddx-signup-page.component';
+export * from './ddx-signup-page/ddx-signup-wrapper/ddx-signup-page.component';
 export * from './ddx-signup-success-page/ddx-signup-success-page.component';
 export * from './ddx-two-factor-signin-page/ddx-two-factor-signin-page.component';
