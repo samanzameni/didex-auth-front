ca46699b5e19ccf891d63672e428bad79d50e2df
Newsha Nik <nioosha.h@gmail.com>
2020-08-17 17:59:04 +0430

    deleted everything from signup wrapper ts

--- src/app/feature/pages/ddx-signup-page/ddx-signup-wrapper/ddx-signup-wrapper.component.ts
+++ src/app/feature/pages/ddx-signup-page/ddx-signup-wrapper/ddx-signup-wrapper.component.ts
@@ -1,19 +1,4 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  Renderer2,
-  AfterViewInit,
-  ChangeDetectorRef,
-} from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { mustMatch, isStrong } from '@core/util/validators';
-import { AuthFormData } from '@core/models';
-import { AuthService } from '@core/services';
-import { Router } from '@angular/router';
-import { AuthPageDirective } from '@feature/templates/ddx-auth-page.template';
-import { MatCheckbox } from '@angular/material/checkbox';
-import { environment } from '@environments/environment';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ddx-signup-wrapper',
@@ -23,77 +8,8 @@ import { environment } from '@environments/environment';
     './ddx-signup-wrapper.component.scss',
   ],
 })
-export class SignUpWrapperComponent extends AuthPageDirective
-  implements OnInit, AfterViewInit {
-  @ViewChild(MatCheckbox) checkbox: MatCheckbox;
+export class SignUpWrapperComponent implements OnInit {
+  constructor() {}
 
-  constructor(
-    protected formBuilder: FormBuilder,
-    protected renderer: Renderer2,
-    protected router: Router,
-    protected authService: AuthService,
-    protected cdRef: ChangeDetectorRef
-  ) {
-    super(formBuilder, renderer, router, authService, cdRef);
-  }
-
-  ngOnInit() {
-    this.authForm = this.formBuilder.group(
-      {
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(8)]],
-        confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
-        acceptedTerms: [false, [Validators.requiredTrue]],
-        token: ['', environment.production ? [Validators.required] : []],
-      },
-      {
-        validators: [
-          mustMatch('password', 'confirmPassword'),
-          isStrong('password'),
-        ],
-      }
-    );
-  }
-
-  ngAfterViewInit(): void {
-    super.ngAfterViewInit();
-  }
-
-  get isFormValid(): boolean {
-    return this.authForm.valid;
-  }
-
-  onSubmit(): void {
-    this.setLoadingOn();
-    this.formErrors = {};
-
-    const { confirmPassword, acceptedTerms, ...formData } = this.authForm.value;
-    this.authService.requestSignUp(formData as AuthFormData).subscribe(
-      (response) => {
-        this.setLoadingOff();
-        this.router.navigateByUrl('/signup/success');
-      },
-      (errorResponse) => {
-        this.setLoadingOff();
-
-        if (errorResponse.status === 400) {
-          const errors = errorResponse.error.errors;
-
-          if (errors.email) {
-            this.formErrors.email = errors.email;
-          }
-
-          if (errors.password) {
-            this.formErrors.password = errors.password;
-          }
-
-          for (const key of Object.keys(errors)) {
-            if (!['email', 'password', 'default'].includes(key)) {
-              alert(`An error occured: There is something wrong with ${key}`);
-            }
-          }
-        }
-      }
-    );
-  }
+  ngOnInit() {}
 }
