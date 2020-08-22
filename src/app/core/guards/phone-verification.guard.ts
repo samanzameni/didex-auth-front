26fd5f20436ad3580be9fc53717b0434f6dfa4f6
Newsha Nik <nioosha.h@gmail.com>
2020-08-22 15:22:03 +0430

    finished phone verification guard

--- src/app/core/guards/phone-verification.guard.ts
+++ src/app/core/guards/phone-verification.guard.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
-  Router,
-} from '@angular/router';
+import { CanActivate, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DdxRegisterDataService } from '@core/services';
 
@@ -17,10 +11,12 @@ export class PhoneVerificationGuard implements CanActivate {
     private userDataService: DdxRegisterDataService,
     private router: Router
   ) {}
-  canActivate() {
+  canActivate(): boolean | UrlTree {
     if (this.userDataService.email && this.userDataService.password) {
       return true;
     } else {
+      //   alert('Enter your email and password in order to proceed.');
+      return this.router.parseUrl('/signup/credentials');
     }
   }
 }
