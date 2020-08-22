8e79e2bcadd3beda3c9c1c38faaa66e3e3ad77fc
Newsha Nik <nioosha.h@gmail.com>
2020-08-22 16:49:23 +0430

    fixed routing problem.

--- src/app/feature/layouts/auth-layout/auth-layout.component.ts
+++ src/app/feature/layouts/auth-layout/auth-layout.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  LocaleService,
+  Locale,
+  LocaleModel,
+  DirectionService,
+  Direction,
+} from '@core/services';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'auth-layout',
@@ -6,7 +14,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-layout.component.scss'],
 })
 export class AuthLayoutComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private localeService: LocaleService,
+    private directionService: DirectionService
+  ) {}
+
+  handleLocaleChange($event: Locale): void {
+    this.localeService.changeLocale($event);
+    window.location.reload();
+  }
+
+  get availableLocales(): LocaleModel[] {
+    return this.localeService.availableLocales;
+  }
+
+  get currentLocaleModel(): LocaleModel {
+    return this.localeService.currentLocaleModel;
+  }
+
+  get direction$(): Observable<Direction> {
+    return this.directionService.direction$;
+  }
+
+  get locale$(): Observable<Locale> {
+    return this.localeService.locale$;
+  }
 
   ngOnInit(): void {}
 }
--- src/app/feature/modules/auth.module.ts
+++ src/app/feature/modules/auth.module.ts
@@ -14,6 +14,7 @@ import {
   ResetPasswordPageComponent,
   ActivateEmailPageComponent,
   TwoFactorSigninPageComponent,
+  AuthLayoutComponent,
 } from '@feature/pages';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -22,13 +23,39 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { LocalePipeModule } from './locale-pipe.module';
 import { PhoneVerificationGuard } from '@core/guards/phone-verification.guard';
+import { MatOption } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'signin',
+    component: AuthLayoutComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'signin' },
+      {
+        path: 'signin',
+        component: SignInPageComponent,
+      },
+      {
+        path: 'signup',
+        component: SignUpWrapperComponent,
+        children: [
+          { path: '', pathMatch: 'full', redirectTo: 'credentials' },
+          { path: 'credentials', component: SignupCredentialsComponent },
+          {
+            path: 'phone-verification',
+            canActivate: [PhoneVerificationGuard],
+            component: SignupPhoneVerificationComponent,
+          },
+        ],
+      },
+    ],
   },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'signin',
+  // },
   {
     path: 'signin',
     component: SignInPageComponent,
@@ -96,6 +123,7 @@ const routes: Routes = [
     MatButtonModule,
     MatCheckboxModule,
     MatIconModule,
+    MatSelectModule,
   ],
   exports: [],
   providers: [],
